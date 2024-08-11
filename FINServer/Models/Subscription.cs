b5a71aafc9f27239a61f3c1781e7853764d289ea
Tarik Kadric <kadric.tarik@hotmail.com>
2024-08-11 14:03:55 +0200

    Created Subscription Class

---
+++ FINServer/Models/Subscription.cs
@@ -0,0 +1,13 @@
+﻿namespace FINServer.Models
+{
+    public class Subscription
+    {
+        public int SubscriptionId { get; set; }
+        public int CustomerId { get; set; }
+        public string ServiceName { get; set; }
+        public decimal MonthlyFee { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Active { get; set; }
+    }
+}
