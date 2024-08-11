a6e61fc7e15aa286c43e3d061b863fbf1d6f9f6c
Tarik Kadric <kadric.tarik@hotmail.com>
2024-08-11 16:55:43 +0200

    Created all Models

---
+++ FINServer/Models/SupportTicket.cs
@@ -0,0 +1,20 @@
+﻿namespace FINServer.Models
+{
+    public class SupportTicket
+    {
+        public int TicketId { get; set; }
+        public int CustomerId { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public TicketStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt{ get; set; }
+    }
+
+    public enum TicketStatus
+    {
+        Resolved,
+        InProgress,
+        Open
+    }
+}
---
+++ FINServer/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿namespace FINServer.Models
+{
+    public class Transaction
+    {
+        public int TransactionID { get; set; }
+        public int AccountID { get; set; }
+        public decimal Amount { get; set; }
+        public TransActionType TransactionType { get; set; }
+        public int TargetAccountId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Description { get; set; }
+    }
+
+    public enum TransActionType
+    {
+        Withdrawal,
+        Deposit,
+        Transfer
+    }
+}
