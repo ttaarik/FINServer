d54e772a04d3559fe73633cfbc03560996a4c082
Tarik Kadric <kadric.tarik@hotmail.com>
2024-08-12 00:00:36 +0200

    Balance wont show up, need to fix bux

---
+++ FINServer/Controllers/TransactionController.cs
@@ -0,0 +1,72 @@
+﻿using FINServer.Data;
+using FINServer.Models;
+using FINServer.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace FINServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TransactionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"SELECT * FROM `transactions`;";
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    MySqlDataReader myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpGet]
+        [Route("id")]
+
+        public JsonResult Get(int id)
+        {
+            string query = $@"SELECT t.transaction_id, t.amount, t.transaction_type, t.timestamp, t.description, 
+                            t.target_account_id, a2.customer_id AS target_customer_id
+                            FROM transactions t
+                            JOIN accounts a ON t.account_id = a.account_id
+                            LEFT JOIN accounts a2 ON t.target_account_id = a2.account_id
+                            WHERE a.customer_id = {id};";
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    MySqlDataReader myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+    }
+}
